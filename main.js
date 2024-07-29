e833d57b7e45ed87ab3523c2c5622cd59a7fb48f
awat <154649552+Away12568@users.noreply.github.com>
2024-07-30 02:52:24 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+let test = prompt ("введите любое число")
+
+if (test < 0){
+    console.log("это отрицательно число");
+}
+
+
+else if (test == 0 ) {
+    console.log("это ноль");
+}  
+
+else if (test >= 1) {
+   console.log("это положительное число"); 
+}
+ 
+let age = prompt("какого вы года рождения") 
+ 
+console.log(2024 - age);
+
+
+
